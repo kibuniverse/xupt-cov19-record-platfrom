ea8408361150ea285e432d102094c7c8c2930f2f
kibuniverse <3050540005@qq.com>
2021-12-28 20:16:30 +0800

    feat: manage user

--- src/pages/Manage/index.tsx
+++ src/pages/Manage/index.tsx
@@ -3,6 +3,7 @@ import { userDelete, userList } from '@/services/ant-design-pro/api';
 import { PageContainer } from '@ant-design/pro-layout';
 import { Button, message, Popconfirm, Table } from 'antd';
 import * as React from 'react';
+import './index.less';
 
 const Manager: React.FC = () => {
   const [list, setList] = React.useState<any[]>([]);
@@ -30,20 +31,24 @@ const Manager: React.FC = () => {
       render: (text: string, record: Record<string, unknown>) => {
         console.log(record);
         return (
-          <div>
-            <Popconfirm
-              title="确认删除该用户吗?"
-              onConfirm={async () => {
-                console.log('删除 ' + record.userId);
-                const ans = await userDelete({ userId: record.userId as number });
-                if (ans.code === RespCodeType.success) {
-                  message.success('删除成功');
-                  fetchUserList();
-                }
-              }}
-            >
-              <Button>删除</Button>
-            </Popconfirm>
+          <div className="table-operate">
+            <div className="item">
+              <Popconfirm
+                title="确认删除该用户吗?"
+                onConfirm={async () => {
+                  console.log('删除 ' + record.userId);
+                  const ans = await userDelete({ userId: record.userId as number });
+                  if (ans.code === RespCodeType.success) {
+                    message.success('删除成功');
+                    fetchUserList();
+                  }
+                }}
+              >
+                <Button danger>删除</Button>
+              </Popconfirm>
+            </div>
+
+            <Button>修改密码</Button>
           </div>
         );
       },
